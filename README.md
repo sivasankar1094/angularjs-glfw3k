a462a332d5fe13b1fca5e1bace0e3fa104fbf14b
sivasankar1094 <ssivasankar1094@gmail.com>
2020-08-15 13:24:09 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angularjs-glfw3k
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angularjs-glfw3k)
---
+++ home/home.controller.js
@@ -0,0 +1,13 @@
+class HomeCtrl {
+  constructor($scope) {
+    'ngInject';
+    this.all= "shocking...."
+    this.country={ 'all' : false , 'CH' : false, 'NL' : false , 'AD' : false } 
+    this.name = "AngularJS";
+  }
+
+
+
+}
+
+export default HomeCtrl;
